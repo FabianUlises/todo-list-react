7b319ea40383845ba4df48b18a37d88885da523c
fabian ulises <ulises.fab@outlook.com>
2024-06-26 23:34:06 -0700

    Handling input change and setting input to value state.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,7 +3,7 @@ const TodoForm = () => {
     const [value, setValue] = useState('');
     return(
         <form>
-            <input type='text' placeholder='Enter new task' className='todo-input' />
+            <input onChange={(e) => setValue(e.target.value)} value={value} type='text' placeholder='Enter new task' className='todo-input' />
             <button type='submit' className='todo-btn'>Add Task</button>
         </form>
     );
