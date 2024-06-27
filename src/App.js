e0ddbcb40310c33285c697bad4e7c30719d45136
fabian ulises <ulises.fab@outlook.com>
2024-06-26 23:48:46 -0700

    Updated App component. Added state to store array of tasks.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { Component, useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 function App() {
+  // State
   const [todos, setTodos] = useState([]);
+  // Function to update state with new task
+  const addTodo = (todo) => {
+    // Updating state
+    setTodos([...todos], {
+
+    });
+  };
   return (
     <div className="App">
       <TodoForm />
