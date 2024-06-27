4e6e80c44abf3f96733013158e0c9461b4ce4681
fabian ulises <ulises.fab@outlook.com>
2024-06-26 23:38:08 -0700

    Updated todoform component. Created function handler to handle form submit.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,14 @@
 import React, { Component, useState } from 'react';
 const TodoForm = () => {
+    // State
     const [value, setValue] = useState('');
+    // Form submit handler
+    const handleSubmit = (e) => {
+        // Prevent browser default
+        e.preventDefault();
+    };
     return(
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <input onChange={(e) => setValue(e.target.value)} value={value} type='text' placeholder='Enter new task' className='todo-input' />
             <button type='submit' className='todo-btn'>Add Task</button>
         </form>
