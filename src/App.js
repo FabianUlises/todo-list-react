54ee5fa56d5fff43f3eeede67de2b1054ba11231
fabian ulises <ulises.fab@outlook.com>
2024-06-26 23:45:20 -0700

    Updated app component. Added state for tasks.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React, { Component, useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <TodoForm />
